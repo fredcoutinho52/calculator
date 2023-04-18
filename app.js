5fed2d2998f619e0c0f2a472dc990f9c929155ba
fredcoutinho52 <fredcoutinho52@gmail.com>
2023-04-18 16:50:05 -0300

    implementando soma

--- app.js
+++ app.js
@@ -1,23 +1,63 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  setDisplay();
+
+  const buttons = document.querySelectorAll(".number-button");
+  buttons.forEach(item => {
+    item.addEventListener("click", (e) => getNumber(e));
+  });
+});
+
+const data = {
+  num: 0,
+  nextNum: 0,
+  operation: "",
+}
+
+function setDisplay() {
+  const display = document.querySelector(".display");
+  display.innerText = data.num;
+}
+
+function getNumber(event) {
+  const value = Number(event.target.innerText);
+  
+  if (data.num !== 0) {
+    data.nextNum = value;
+    setDisplay();
+    return;
+  }
+  
+  data.num = value;
+  setDisplay();
+}
+
 function sum() {
-  console.log('teste');
+  data.operation = "+";
+
+  if (data.num !== 0 && data.nextNum !== 0) {
+    console.log(data.num + data.nextNum);
+    data.num = data.num + data.nextNum;
+    return;
+  }
 }
 
 function sub() {
-
+  data.operation = "-";
 }
 
 function times() {
-
+  data.operation = "*";
 }
 
 function divide() {
-
+  data.operation = "/";
 }
 
 function equal() {
-
+  data.operation = "=";
 }
 
 function del() {
-
+  
 }
